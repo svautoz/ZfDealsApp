a0208c6397889df8f0f1bf4131fc9b42d9bd21e1
sferz <info@svautoz.ru>
2017-08-17 13:23:23 +0300

    Копия проекта

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,7 +11,7 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
-    'Zend\Router',
-    'Zend\Validator',
     'Application',
+    'ZFTool',
+    'ZFDeals',
 ];
